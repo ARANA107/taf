adce3f19e0dada08cf2dbd003e0a569f3d6307f7
Ash Rana <ashrana1993@gmail.com>
2020-09-20 01:08:52 -0500

    react-create-app on Heroku

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>this is app!</h1>
+      <img style={{ width: "100px" }} src={"assets/images/car1.jpg"} alt="image" />
+    </div>
+  );
+}
+
+export default App;
